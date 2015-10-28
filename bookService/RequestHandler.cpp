b6db71c985d67f191fac87f82f482a991a89bec9
user <user@ubuntu.(none)>
2015-10-28 22:52:38 +0300

    request handler class

---
+++ bookService/RequestHandler.cpp
@@ -0,0 +1,34 @@
+#include <fastcgi2/component.h>
+#include <fastcgi2/component_factory.h>
+#include <fastcgi2/handler.h>
+#include <fastcgi2/request.h>
+#include "fastcgi2/stream.h"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
+#include <stdio.h>
+
+class RequestHandler : virtual public fastcgi::Component, virtual public fastcgi::Handler {
+
+public:
+        RequestHandler(fastcgi::ComponentContext *context) :
+                fastcgi::Component(context) {
+        }
+        virtual ~RequestHandler() {
+        }
+
+public:
+        virtual void onLoad() {
+        }
+        virtual void onUnload() {
+        }
+        virtual void handleRequest(fastcgi::Request *req, fastcgi::HandlerContext *context) {
+
+        }
+
+};
+
+FCGIDAEMON_REGISTER_FACTORIES_BEGIN()
+FCGIDAEMON_ADD_DEFAULT_FACTORY("simple_factory", RequestHandler)
+FCGIDAEMON_REGISTER_FACTORIES_END()
